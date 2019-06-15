50f3c3f1af3d3ae993c0957c545f447d83e516a4
haticenurokur <haticenurokur@gmail.com>
2019-06-15 22:03:24 +0300

    examples added

---
+++ app/src/main/java/com/androidexample/ExampleFragment.java
@@ -0,0 +1,75 @@
+package com.androidexample;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+public class ExampleFragment extends Fragment {
+
+    @BindView(R.id.tvMessage)
+    TextView tvMessage;
+
+    @BindView(R.id.btnChangeText)
+    Button btnChangeText;
+
+    @BindView(R.id.btnSecondFragment)
+    Button btnSecondFragment;
+
+    private int counter;
+
+
+    public ExampleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_example, container, false);
+
+        ButterKnife.bind(this,view);
+
+        counter = 0;
+
+        return view;
+    }
+
+    @OnClick(R.id.btnChangeText)
+    public void changeText(){
+
+        ++counter;
+
+        String displayMsg = "Deneme " + counter;
+
+        tvMessage.setText(displayMsg);
+
+    }
+
+    @OnClick(R.id.btnSecondFragment)
+    public void openSecondFragment(){
+
+        // Yeni Fragment aşağıdaki 4 satır kod ile açılır
+
+        // Fragmenlarda activity metodu çağırmak için başına getActivity() yazılır.
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_frame, new SecondFragment());
+        transaction.addToBackStack(null);
+        transaction.commitAllowingStateLoss();
+
+    }
+
+}
--- app/src/main/java/com/androidexample/MainActivity.java
+++ app/src/main/java/com/androidexample/MainActivity.java
@@ -1,7 +1,8 @@
 package com.androidexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Yeni Fragment aşağıdaki 4 satır kod ile açılır
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_frame, new ExampleFragment());
+        transaction.disallowAddToBackStack();
+        transaction.commitAllowingStateLoss();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        int count = getSupportFragmentManager().getBackStackEntryCount();
+
+        if (count == 0) {
+            super.onBackPressed();
+            //additional code
+        } else {
+            getSupportFragmentManager().popBackStack();
+        }
+
     }
 }
